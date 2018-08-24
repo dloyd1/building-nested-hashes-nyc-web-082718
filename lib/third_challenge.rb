452b3c34a56b9c2dc6f5adb1075198cd67d69227
Daniel Loyd <dhloyd@icloud.com>
2018-08-24 13:51:42 +0000

    automatically backed up by learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -1,6 +1,31 @@
 def third_challenge
-
-  epic_tragedy = {
+  # epic_tragedy = {
+  #  :montague => {
+  #     :patriarch => {
+  #       #your key/value pairs here
+  #       },
+  #     :matriarch => {
+  #       #your key/value pairs here
+  #       },
+  #     :hero => {
+  #       #your key/value pairs here
+  #       },
+  #     :hero_friends => []
+  #  },
+  #  :capulet => {
+  #     :patriarch => {
+  #       #your key/value pairs here
+  #       },
+  #     :matriarch => {
+  #       #your key/value pairs here
+  #       },
+  #     :heroine => {
+  #       #your key/value pairs here
+  #       },
+  #     :heroine_friends => []
+  #  }
+  # }
+   epic_tragedy = {
    :montague => {
       :patriarch => {name: "Lord Montague", age: "53"},
       :matriarch => {name: "Lady Montague", age: "54"},
@@ -13,5 +38,5 @@ def third_challenge
       :heroine => {name: "Juliette", age: "15", status: "alive"},
       :heroine_friends => []
    }
-  }
+}
 end
